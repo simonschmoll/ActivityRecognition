fe54ddad2a4800d9d2b4af3c2d4a129f01c4c33b
alesssandro <alessandro.pomes@gmail.com>
2017-09-30 15:48:27 +0200

    read data

--- ActivityRecognition.py
+++ ActivityRecognition.py
@@ -9,3 +9,12 @@ while(num < 16):
     mylist.append(data)
     num += 1
 print(mylist[0])
+
+from pandas import read_csv
+import numpy
+num = 1
+while(num < 16):
+     dataset = read_csv('data/%d.csv' % (num), header=None)
+     num += 1
+
+     print(dataset.describe())
--- Miss_data.py
+++ Miss_data.py
@@ -4,6 +4,7 @@ num = 1
 while(num < 16):
      dataset = read_csv('data/%d.csv' % (num), header=None)
      num += 1
+
      print(dataset.describe())
    # dataset = read_csv('data/%d.csv' % (num), header=None)
 
