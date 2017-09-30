e7944f0d579eb29ef5a5913ebc8da18d28e78a1e
alesssandro <alessandro.pomes@gmail.com>
2017-09-30 16:32:19 +0200

    Read data 1

--- ActivityRecognition.py
+++ ActivityRecognition.py
@@ -1,20 +1,15 @@
-import sklearn as metrics
-import numpy as np
-from numpy import genfromtxt
-data1 = genfromtxt('data/1.csv', delimiter=',')
-mylist = []
-num = 1
-while(num < 16):
-    data = genfromtxt('data/%d.csv' % (num), delimiter=',')
-    mylist.append(data)
-    num += 1
-print(mylist[0])
-
 from pandas import read_csv
+import pandas as pd
 import numpy
 num = 1
+names_attributes = ['sequentialNumber','xAcceleration','yAcceleration','zAcceleration','lable']
+
 while(num < 16):
-     dataset = read_csv('data/%d.csv' % (num), header=None)
+     table = pd.read_csv('data/%d.csv' % (num), sep=',', index_col=False)
+     if num == 1:
+         all_tables = table
+     else:
+         all_tables = pd.concat([all_tables, table])
      num += 1
 
-     print(dataset.describe())
+print(all_tables)
--- Miss_data.py
+++ Miss_data.py
@@ -1,11 +1,18 @@
 from pandas import read_csv
-import numpy
+import numpy as np
 num = 1
 while(num < 16):
      dataset = read_csv('data/%d.csv' % (num), header=None)
+     dataArray = dataset.as_matrix()
+     if num == 1:
+          allData = dataArray
+     else:
+         np.append(allData, dataArray)
      num += 1
 
-     print(dataset.describe())
+print(allData)
+
+
    # dataset = read_csv('data/%d.csv' % (num), header=None)
 
     #print(dataset.head(20))
