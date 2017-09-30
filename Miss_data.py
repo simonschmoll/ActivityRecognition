e2d3d6d4f1f85ced34105f66ec245f9d0a6c56cc
alesssandro <alessandro.pomes@gmail.com>
2017-09-30 17:58:53 +0200

    change import of data from csv

--- Miss_data.py
+++ Miss_data.py
@@ -1,14 +1,12 @@
 from pandas import read_csv
 import numpy as np
-num = 1
+#header= ['sequentialNumber','xAcceleration','yAcceleration','zAcceleration','lable']
+num = 2
+allData=read_csv('data/1.csv', header=None)
 while(num < 16):
-     dataset = read_csv('data/%d.csv' % (num), header=None)
-     dataArray = dataset.as_matrix()
-     if num == 1:
-          allData = dataArray
-     else:
-         np.append(allData, dataArray)
-     num += 1
+    dataset = read_csv('data/%d.csv' % (num), header=None)
+    allData = allData.append(dataset,ignore_index=False,verify_integrity=False)
+    num += 1
 
 print(allData)
 
