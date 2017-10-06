92d4f723b69d081283183a53254f3ea90def2cc8
simonschmoll <simon.schmoll@gmail.com>
2017-10-06 17:27:45 +0200

    moved code to the right files

--- ActivityRecognition.py
+++ ActivityRecognition.py
@@ -4,12 +4,17 @@ import numpy
 num = 1
 names_attributes = ['sequentialNumber','xAcceleration','yAcceleration','zAcceleration','lable']
 
-while(num < 16):
-     table = pd.read_csv('data/%d.csv' % (num), sep=',', index_col=False)
-     if num == 1:
-         all_tables = table
-     else:
-         all_tables = pd.concat([all_tables, table])
-     num += 1
+#while (num < 16):
+dataset = read_csv('data/%d.csv' % (num), header=None)
+    # if num == 1:
+    #     allData = dataset
+    # else:
+    #     allData = allData.append(dataset)
+    #     print('This is all Data at point', num, allData)
+    #num += 1
+print(dataset)
 
-print(all_tables)
+#Feature Extraction
+#Mean Value
+#feat_meanValue = np.mean(allData)
+#gitprint(feat_meanValue)
--- Miss_data.py
+++ Miss_data.py
@@ -1,19 +1,3 @@
 from pandas import read_csv
 import pandas as pd
 import numpy as np
-
-def read_data_set():
-    num = 1
-    while(num < 16):
-         dataset = read_csv('data/%d.csv' % (num), header=None)
-         if num == 1:
-              allData = dataset
-         else:
-             allData = allData.append(dataset)
-             print('This is all Data at point',num, allData)
-         num += 1
-
-#Feature Extraction
-#Mean Value
-#feat_meanValue = np.mean(allData)
-#gitprint(feat_meanValue)
