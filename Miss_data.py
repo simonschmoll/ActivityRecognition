5b8b441317bc48fa82f062e89a607b2b42dee0d0
simonschmoll <simon.schmoll@gmail.com>
2017-10-06 15:38:58 +0200

    new definition for method and beginning of feature extraction

--- Miss_data.py
+++ Miss_data.py
@@ -1,16 +1,19 @@
 from pandas import read_csv
+import pandas as pd
 import numpy as np
-#header= ['sequentialNumber','xAcceleration','yAcceleration','zAcceleration','lable']
-num = 2
-allData=read_csv('data/1.csv', header=None)
-while(num < 16):
-    dataset = read_csv('data/%d.csv' % (num), header=None)
-    allData = allData.append(dataset,ignore_index=False,verify_integrity=False)
-    num += 1
 
-print(allData)
+def read_data_set():
+    num = 1
+    while(num < 16):
+         dataset = read_csv('data/%d.csv' % (num), header=None)
+         if num == 1:
+              allData = dataset
+         else:
+             allData = allData.append(dataset)
+             print('This is all Data at point',num, allData)
+         num += 1
 
-
-   # dataset = read_csv('data/%d.csv' % (num), header=None)
-
-    #print(dataset.head(20))
+#Feature Extraction
+#Mean Value
+#feat_meanValue = np.mean(allData)
+#gitprint(feat_meanValue)
