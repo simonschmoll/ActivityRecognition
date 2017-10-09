c80d9cb1ec0ea7ce9d1f0c7074876c40ce5cc6fe
simonschmoll <simon.schmoll@gmail.com>
2017-10-09 10:46:33 +0200

    commented out not yet working code

--- ActivityRecognition.py
+++ ActivityRecognition.py
@@ -79,9 +79,9 @@ print(feature)
 # now we need to learn a random forest classifier (best fit according to  "Pierluigi Casale, Oriol Pujol, and Petia Radeva. Human activity recognition from accelerometer
 # data using a wearable device. Pattern Recognition and Image Analysis, pages 289–296, 2011.
 # [4] Wenchao Jiang and Zhaozheng Yin. Human activity recognition using wearable sensors by")
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_predict
-classifier = RandomForestClassifier()
-prediction = cross_val_predict(classifier, feature, target, cv=5)
+# from sklearn.model_selection import cross_val_score
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.model_selection import cross_val_predict
+# classifier = RandomForestClassifier()
+# prediction = cross_val_predict(classifier, feature, target, cv=5)
 
