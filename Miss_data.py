1dfd3ab3092e1d8bf0c35f57d00236bacbf94e6f
alesssandro <alessandro.pomes@gmail.com>
2017-10-12 17:13:30 +0200

    MissData

--- Miss_data.py
+++ Miss_data.py
@@ -1,3 +1,30 @@
 from pandas import read_csv
-import pandas as pd
 import numpy as np
+header= ['sequentialNumber','xAcceleration','yAcceleration','zAcceleration','lable']
+num = 1
+#allData=read_csv('data/1.csv', header=None)
+while(num < 16):
+#  #   dataset = read_csv('data/%d.csv' % (num), header=None)
+#  #   allData = allData.append(dataset,ignore_index=False,verify_integrity=False)
+    dataset = read_csv('data/%d.csv' % (num) , names=header)
+    boolData = dataset.isnull()
+    for idx in header:
+       print(boolData.loc[boolData[idx] == True])
+    num += 1
+    # print(dataset.describe())
+ #print(allData)
+
+ #define the function for  delete rows with 0 Labels
+ # @input: Pandas Dataframe, value of lable to delet
+ # @output: Array of int without lable 0
+def zeroDet( dataset, value ):
+    num = []
+    num=(dataset.loc[dataset['lable'] == value].index.values)
+    dataClean=(dataset.drop(num))
+    dataClean = dataClean.as_matrix()
+    dataClean = dataClean.astype(np.int)
+    return dataClean;
+
+dataset=read_csv('data/1.csv', names=header)
+dataArray=zeroDet(dataset,0)
+print(dataArray)
