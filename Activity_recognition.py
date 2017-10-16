fd8335327803b6adea6eb0913a091022fe1af3d2
simonschmoll <simon.schmoll@gmail.com>
2017-10-16 17:50:35 +0200

    minor changes in code sequences, refactoring

--- Activity_recognition.py
+++ Activity_recognition.py
@@ -12,16 +12,18 @@ dataframe = read_convert(1)
 # Just now I will convert by hand
 array_data = dataframe.as_matrix()
 array_data = array_data.astype(np.int)
+print(np.size(array_data,0))
 
 #Feature Extraction
 grouped_data = grouping(array_data)
+print(np.size(grouped_data,0))
 features = extract_features(grouped_data)
 
 #Feature Selection
 print(features.__getitem__(0))
 selected_feature = select_best_feature(features.__getitem__(0), features.__getitem__(1))
 print(selected_feature.__class__)
-#To verify our results we print the size of the dataList--> as it are 162500 rows we should have (162500/26)
+# To verify our results we print the size of the dataList--> as it are 162500 rows we should have (162500/26)
 # print(np.size(dataList))
 # print(dataList)
 
--- Miss_data.py
+++ Miss_data.py
@@ -1,17 +1,5 @@
-from pandas import read_csv
 import numpy as np
 header= ['sequentialNumber','xAcceleration','yAcceleration','zAcceleration','lable']
-# num = 1
-#allData=read_csv('data/1.csv', header=None)
-# while(num < 16):
-#  #   dataset = read_csv('data/%d.csv' % (num), header=None)
-#  #   allData = allData.append(dataset,ignore_index=False,verify_integrity=False)
-#     dataset = read_csv('data/%d.csv' % (num) , names=header)
-#     boolData = dataset.isnull()
-#     for idx in header:
-#        print(boolData.loc[boolData[idx] == True])
-#     num += 1
- #print(allData)
 
  #define the function for  delete rows with 0 Labels
  # @input: Pandas Dataframe, value of lable to delet
@@ -24,6 +12,3 @@ def zeroDet( dataset, value ):
     dataClean = dataClean.astype(np.int)
     return dataClean
 
-# dataset=read_csv('data/1.csv', names=header)
-# dataArray=zeroDet(dataset,0)
-# print(dataArray)
