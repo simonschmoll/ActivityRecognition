94ca5cfc581688543fee51f45c28c2740c0e15a6
alesssandro <alessandro.pomes@gmail.com>
2017-10-19 02:43:51 +0200

    modificed estimetor parameters and create a function of plot

--- Classification.py
+++ Classification.py
@@ -11,7 +11,11 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 
 #function to classify without crossvalidaion:
-#we created here funtion to pre
+#we created here funtion to predict with Random forest and SVM the values with a simple division of the dataset.
+#we split infact the dataset in 2 part:Training and test set and we provide the follow estimators:
+#1)F1score
+#2)Accuracy
+#3)Confusion Matrix
 
 def classify(x_features, y_features):
     X_train, X_test, y_train, y_test = train_test_split(x_features, y_features.ravel(), test_size=0.2, random_state=0)
@@ -23,10 +27,21 @@ def classify(x_features, y_features):
     modelSv=clf.fit(X_train,y_train )
     predicted_labels = model.predict(X_test)
     predicted_labelsSv = modelSv.predict(X_test)
+
+    # Compute the F1 score, also known as balanced F-score or F-measure
+    # The F1 score can be interpreted as a weighted average of the precision and recall,
+    #  where an F1 score reaches its best value at 1 and worst score at 0.
+    #  The relative contribution of precision and recall to the F1 score are equal.
+    #  The formula for the F1 score is:
+    # F1 = 2 * (precision * recall) / (precision + recall)
+    # In the multi-class and multi-label case, this is the weighted average of the F1 score of each class.
+
     print(" F1 score Random forest: %f" % f1_score(y_test, predicted_labels, average='macro'))
     print(" F1 score precision with SV: %f" % f1_score(y_test, predicted_labels, average='macro'))
     print("Accuracy Random forest: %f" % metrics.accuracy_score(y_test, predicted_labels))
     print("Accuracy with SV: %f" % metrics.accuracy_score(y_test, predicted_labelsSv))
+    #By definition a confusion matrix C is such that C_{i, j}
+    # is equal to the number of observations known to be in group i but predicted to be in group j.
     print("Confusion Matrix Random forest: ")
     print(confusion_matrix(y_test, predicted_labels, labels=[1, 2, 3, 4, 5, 6, 7]))
     print("Confusion Matrix with SV: ")
